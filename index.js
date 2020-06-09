a2db9f14d98c77ef928d7aa3c763ff65e6d69ba5
eminacar47 <66651096+eminacar47@users.noreply.github.com>
2020-06-09 08:57:18 +0300

    problem27

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
-var baseNumbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+var factors = {};
 
-var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-
-var baseTens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-
-var countLettersCountingToOneThousand = function() {
-    var count = 0;
-
-    //1-9
-    for (var i = 0; i < baseNumbers.length; i++) {
-        var baseNumber = baseNumbers[i];
-        count += baseNumber.length;
-    }
-
-    //10-19
-    for (var i = 0; i < teens.length; i++) {
-        var teen = teens[i];
-        count += teen.length;
-    }
-
-    //20-99
-    for (var i = 0; i < baseTens.length; i++) {
-        var baseTen = baseTens[i];
-        for (var j = 0; j < baseNumbers.length; j++) {
-            var baseNumber = baseNumbers[j];
-            count += baseTen.length + baseNumber.length;
+factors.getFactors = function (number){
+    var factors = [];
+    
+    var possibleFactor = 1;
+    var sqrt = Math.sqrt(number);
+    while(possibleFactor <= sqrt){
+        if(number % possibleFactor == 0){
+            factors[factors.length] = possibleFactor;
+            
+            var otherPossibleFactor = number / possibleFactor;
+            if(otherPossibleFactor > possibleFactor){
+                factors[factors.length] = otherPossibleFactor;
+            } 
         }
+        possibleFactor++;
     }
-
-    var oneToOneHundred = count;
-
-    //100-999
-    for (var i = 1; i < baseNumbers.length; i++) {
-        var hundredsPrefix = baseNumbers[i] + "hundred";
-
-        count += hundredsPrefix.length;
-        count += oneToOneHundred;
-        count += 99 * (hundredsPrefix + "and").length;
-    }
-
-    //1000
-    count += "onethousand".length;
-
-    return count;
+    
+    return factors;
 };
 
-console.log(countLettersCountingToOneThousand());
+var prime = {};
+
+prime.isPrime = function(num){
+    num = (num > 0) ? num : num * -1;
+    return factors.getFactors(num).length <= 2;
+};
+
+var getNumberOfPrimes = function(a, b){
+    var n = 0;
+    var answer = 1;
+    while(prime.isPrime(n*n + a * n + b)){
+        n++;
+    }
+    
+    return n;
+};
+
+console.log(getNumberOfPrimes(1, 41));
+console.log(getNumberOfPrimes(-79, 1601));
+
+var maxToTest = 1000;
+var maxA = 0;
+var maxB = 0;
+var maxNumPrimes = 0;
+for(var b = -maxToTest; b <= maxToTest ; b++){
+    if(prime.isPrime(b)){
+        for(var a = -maxToTest; a <= maxToTest; a++){
+            var numPrimes = getNumberOfPrimes(a, b);
+            if(numPrimes > maxNumPrimes){
+                maxA = a;
+                maxB = b;
+                maxNumPrimes = numPrimes;
+            }
+        }
+    }
+}
+
+console.log('a: ' + maxA );
+console.log('b: ' + maxB );
+console.log('a*b: ' + (maxA * maxB));
+console.log('maxNumPrimes: ' + maxNumPrimes);
