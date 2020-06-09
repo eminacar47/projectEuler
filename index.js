4d980bf9bde840ed603e3a7795cbf3030a5ba928
eminacar47 <66651096+eminacar47@users.noreply.github.com>
2020-06-09 08:50:07 +0300

    problem17

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-function isPrime(n) {
-    for (var i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i ==0) {
-            return false;
-        }
-    
-    }
-}
+var baseNumbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-function nthPrime(n) {
-    let count = 0, inc = 2;
-    while (count < n) {
-        if (isPrime(inc)) {
-            count++;
-            console.log(count)
-        }
-        inc++;
-    }
-    return inc - 1;
-}
+var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 
-console.log(nthPrime(1))
+var baseTens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+var countLettersCountingToOneThousand = function() {
+    var count = 0;
+
+    //1-9
+    for (var i = 0; i < baseNumbers.length; i++) {
+        var baseNumber = baseNumbers[i];
+        count += baseNumber.length;
+    }
+
+    //10-19
+    for (var i = 0; i < teens.length; i++) {
+        var teen = teens[i];
+        count += teen.length;
+    }
+
+    //20-99
+    for (var i = 0; i < baseTens.length; i++) {
+        var baseTen = baseTens[i];
+        for (var j = 0; j < baseNumbers.length; j++) {
+            var baseNumber = baseNumbers[j];
+            count += baseTen.length + baseNumber.length;
+        }
+    }
+
+    var oneToOneHundred = count;
+
+    //100-999
+    for (var i = 1; i < baseNumbers.length; i++) {
+        var hundredsPrefix = baseNumbers[i] + "hundred";
+
+        count += hundredsPrefix.length;
+        count += oneToOneHundred;
+        count += 99 * (hundredsPrefix + "and").length;
+    }
+
+    //1000
+    count += "onethousand".length;
+
+    return count;
+};
+
+console.log(countLettersCountingToOneThousand());
