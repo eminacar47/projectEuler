e9030aae3e2dd579e84766a4c5e823165942eebf
eminacar47 <66651096+eminacar47@users.noreply.github.com>
2020-06-09 09:16:30 +0300

    problem77

--- index.js
+++ index.js
@@ -1,63 +1,46 @@
-var factors = {};
+var p = [2, 3];
 
-factors.getFactors = function (number){
-    var factors = [];
-    
-    var possibleFactor = 1;
-    var sqrt = Math.sqrt(number);
-    while(possibleFactor <= sqrt){
-        if(number % possibleFactor == 0){
-            factors[factors.length] = possibleFactor;
-            
-            var otherPossibleFactor = number / possibleFactor;
-            if(otherPossibleFactor > possibleFactor){
-                factors[factors.length] = otherPossibleFactor;
-            } 
-        }
-        possibleFactor++;
-    }
-    
-    return factors;
-};
-
-var prime = {};
-
-prime.isPrime = function(num){
-    num = (num > 0) ? num : num * -1;
-    return factors.getFactors(num).length <= 2;
-};
-
-var getNumberOfPrimes = function(a, b){
-    var n = 0;
-    var answer = 1;
-    while(prime.isPrime(n*n + a * n + b)){
-        n++;
-    }
-    
-    return n;
-};
-
-console.log(getNumberOfPrimes(1, 41));
-console.log(getNumberOfPrimes(-79, 1601));
-
-var maxToTest = 1000;
-var maxA = 0;
-var maxB = 0;
-var maxNumPrimes = 0;
-for(var b = -maxToTest; b <= maxToTest ; b++){
-    if(prime.isPrime(b)){
-        for(var a = -maxToTest; a <= maxToTest; a++){
-            var numPrimes = getNumberOfPrimes(a, b);
-            if(numPrimes > maxNumPrimes){
-                maxA = a;
-                maxB = b;
-                maxNumPrimes = numPrimes;
+function prime(max) {
+    var n = 5;
+    while (n <= max) {
+        var f = 1;
+        for (var i = 0; i < p.length; i++) {
+            if (p[i] * p[i] > n) break;
+            if (n % p[i] == 0) {
+                f = 0;
+                break;
             }
         }
+        if (f) {
+            p.push(n);
+        }
+        n = n + 2;
     }
 }
 
-console.log('a: ' + maxA );
-console.log('b: ' + maxB );
-console.log('a*b: ' + (maxA * maxB));
-console.log('maxNumPrimes: ' + maxNumPrimes);
+function sub(n, i) {
+    var a = 0;
+    if (n == 0) return 1;
+    if (i < 0) return 0;
+    while (n >= 0) {
+        a = a + sub(n, i - 1);
+        n = n - p[i];
+    }
+    return a;
+}
+
+function main(num) {
+    prime(num);
+    var n = 2;
+    var m = 0;
+    for (;;) {
+        while (p[m] < n) {
+            m = m + 1;
+        }
+        var c = sub(n, m);
+        if (c >= num) return n;
+        n = n + 1;
+    }
+}
+
+console.log('ANS: ' + main(5000));
